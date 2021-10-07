a38ff0097644dd5bd1240c6bcf175a560df08a83
s113nt <buranbaev.den@mail.ru>
2021-10-07 22:37:46 +0500

    Первые глобальные изменения (новая функция и работа базы данных)

--- main.py
+++ main.py
@@ -3,19 +3,16 @@ import sqlite3
 conn = sqlite3.connect('database.db')
 cursor = conn.cursor()
 
-parking_spaces_1 = []
-parking_spaces_2 = []
-parking_spaces_3 = []
 
-
-def filling_the_array(spaces_count, name_of_array):
-    print('Введите цвет и марку автомобиля (ч/з пробел):')
+def insert_into_db(row, spaces_count):
+    global date, time
+    print('Введите марку и цвет автомобиля (ч/з пробел):')
     for i in range(spaces_count):
-        print('Parking space -', i+1)
-        row = input().split()
-        for i in range(len(row)):
-            row[i] = str(row[i])
-        name_of_array.append(row)
+        print('Parking space -', i + 1)
+        brand, color = input().split()
+        values = (row, i + 1, brand, color, date, time)
+        cursor.execute('INSERT INTO parking_space VALUES (?, ?, ?, ?, ?, ?)', values)
+        conn.commit()
 
 # cursor.execute("""CREATE TABLE parking_space
 #                 (row integer, col integer, brand text, color text, date text, time text)
@@ -23,16 +20,16 @@ def filling_the_array(spaces_count, name_of_array):
 
 
 print('Введите текущюю дату и время.')
-print('Дата в формате DD-MM-YYYY:')
+print('Дата в формате DD-MM-YYYY')
 date = str(input())
 
-print('Время в формате HH:MM:')
+print('Время в формате HH:MM')
 time = str(input())
 
 # Начальные значения количества рядов на парковке [19, 10, 17]
 print('Запись первого ряда.')
-filling_the_array(1, parking_spaces_1)
+insert_into_db(1, 1)
 print('Запись второго ряда.')
-filling_the_array(2, parking_spaces_2)
+insert_into_db(2, 2)
 print('Запись третьего ряда.')
-filling_the_array(3, parking_spaces_3)
+insert_into_db(3, 3)
